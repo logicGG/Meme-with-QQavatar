f8de8a57ca19773dd94c5f3c434b3ba9f18d197d
黄宏基 <huanghj@gditsec.org.cn>
2023-09-21 14:45:51 +0800

    update .gitignore

--- App.py
+++ App.py
@@ -1,15 +1,18 @@
-from flask import Flask, send_file, Response
+from flask import Flask, send_file
+from flask_cors import CORS
 
 from MemeTool import MemeTool
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 tool = MemeTool()
 
 
 @app.route('/meme/<play>/<qq>', methods=['GET'])
 def getMeme(play, qq):
-    resultPath = tool.getNeed(qq)
-    return send_file(resultPath, mimetype='image/png')
+    resultPath = tool.drawNeed(qq)
+    return send_file(resultPath)
 
 
 if __name__ == '__main__':
--- MemeTool.py
+++ MemeTool.py
@@ -1,7 +1,7 @@
 import os
-import requests
 import time
-import random
+
+import requests
 from PIL import Image, ImageDraw
 
 
@@ -17,17 +17,17 @@ class MemeTool:
         if not os.path.exists(self.avatarDirPath):
             os.mkdir(self.avatarDirPath)
 
-    def getAvatar(self, QQ, size, needClipToCircle: bool = True):
+    def getAvatar(self, qq, size, needClipToCircle: bool = True):
         """
         获取头像
-        :param QQ: qq号
+        :param qq: qq号
         :param size: 头像规格
         :param needClipToCircle: 是否转换成圆形，默认为否
         :return: 头像
         """
 
-        if QQ.isdigit():
-            theQQ = int(QQ)
+        if qq.isdigit():
+            theQQ = int(qq)
         else:
             return None
 
@@ -48,8 +48,21 @@ class MemeTool:
             avatar.putalpha(circle)  # 白色区域透明可见，黑色区域不可见
         return avatar
 
-    def getNeed(self, QQ):
-        avatar = self.getAvatar(QQ, (113, 113), needClipToCircle=False)
+    def getDraw(self, playType, qq):
+        if playType == 'need':
+            resultPath = self.drawNeed(qq)
+        elif playType == 'suck':
+            resultPath = self.drawSuck(qq)
+        elif playType == 'tie':
+            resultPath = self.drawTie(qq)
+        elif playType == 'bounce':
+            resultPath = self.drawBounce(qq)
+        else:
+            return None
+        return resultPath
+
+    def drawNeed(self, qq):
+        avatar = self.getAvatar(qq, (113, 113), needClipToCircle=False)
         if avatar is None:
             return None
         frontImage = Image.open(os.path.join(self.drawPath, "need.png"))
@@ -60,7 +73,69 @@ class MemeTool:
         need.save(resultPath)
         return resultPath
 
+    def drawSuck(self, qq):
+        avatar = self.getAvatar(qq, (300, 300))
+        if avatar is None:
+            return None
+        inhalePath = os.path.join(self.drawPath, "inhale")
+        inhaleImages = []
+        positions = [(65, 88), (61, 89), (60, 112), (70, 142), (68, 151), (70, 129), (73, 141), (69, 145), (70, 154),
+                     (68, 119), (64, 115), (64, 99)]
+        sizes = [(162, 151), (167, 146), (165, 120), (151, 90), (151, 84), (149, 109), (145, 94), (151, 89), (149, 76),
+                 (152, 118), (160, 121), (161, 140)]
+        for i in range(0, 12):
+            frontImage = Image.open(os.path.join(inhalePath, "inhale_{}.png".format(i + 1)))
+            img = Image.new("RGBA", frontImage.size, (255, 255, 255))
+            theAvatar = avatar.resize(sizes[i])
+            img.paste(theAvatar, positions[i], theAvatar.split()[3])
+            img.paste(frontImage, (0, 0), frontImage.split()[3])
+            inhaleImages.append(img)
+        resultPath = os.path.join(self.resultDirPath, "{}.gif".format(time.time()))
+        inhaleImages[0].save(resultPath, format="GIF", append_images=inhaleImages[1:], save_all=True, duration=60,
+                             loop=0)
+        return resultPath
+
+    def drawTie(self, qq):
+        avatar = self.getAvatar(qq, (300, 300))
+        if avatar is None:
+            return None
+        snugglePath = os.path.join(self.drawPath, "snuggle")
+        snuggleImages = []
+        positions = [(77, 257), (82, 271), (82, 271), (81, 261), (64, 243)]
+        sizes = [(174, 183), (175, 169), (175, 169), (175, 178), (194, 193)]
+        for i in range(0, 5):
+            frontImage = Image.open(os.path.join(snugglePath, "snuggle_{}.png".format(i + 1)))
+            img = Image.new("RGBA", frontImage.size, (255, 255, 255))
+            theAvatar = avatar.resize(sizes[i])
+            img.paste(theAvatar, positions[i], theAvatar.split()[3])
+            img.paste(frontImage, (0, 0), frontImage.split()[3])
+            snuggleImages.append(img)
+        resultPath = os.path.join(self.resultDirPath, "{}.gif".format(time.time()))
+        snuggleImages[0].save(resultPath, format="GIF", append_images=snuggleImages[1:], save_all=True, duration=110,
+                              loop=0)
+        return resultPath
+
+    def drawBounce(self, qq):
+        avatar = self.getAvatar(qq, (100, 100))
+        if avatar is None:
+            return None
+        bouncePath = os.path.join(self.drawPath, "bounce")
+        bounceImages = []
+        positions = [(103, 51), (103, 46), (101, 10), (101, 27), (103, 46)]
+        sizes = [(35, 35), (35, 35), (39, 35), (38, 37), (35, 35)]
+        for i in range(0, 5):
+            frontImage = Image.open(os.path.join(bouncePath, "bounce_{}.png".format(i + 1)))
+            img = Image.new("RGBA", frontImage.size, (255, 255, 255))
+            theAvatar = avatar.resize(sizes[i])
+            img.paste(theAvatar, positions[i], theAvatar.split()[3])
+            img.paste(frontImage, (0, 0), frontImage.split()[3])
+            bounceImages.append(img)
+        resultPath = os.path.join(self.resultDirPath, "{}.gif".format(time.time()))
+        bounceImages[0].save(resultPath, format="GIF", append_images=bounceImages[1:], save_all=True, duration=70,
+                             loop=0)
+        return resultPath
+
 
 if __name__ == '__main__':
     tool = MemeTool()
-    tool.getNeed('781827926').show()
+    Image.open(tool.getDraw('bounce','781827926')).show()
